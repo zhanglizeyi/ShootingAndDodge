9c0ce33cb96ce9cdcddf9fa1d7a4ae3ef5e492b3
Ze Li <zhanglizeyi@gmail.com>
2015-08-27 15:35:46 -0700

    Shooting and Dodge

--- GamePro/GameScene.swift
+++ GamePro/GameScene.swift
@@ -9,16 +9,59 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  
+  
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        let player = SKSpriteNode(imageNamed: "player")
+        //myLabel.text = "Hello, World!";
+        //myLabel.fontSize = 45;
+        //myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        backgroundColor = SKColor.whiteColor()
+      player.position = CGPoint(x: size.width*0.1,y: size.height*0.5)
+        self.addChild(player)
+      
+      runAction(SKAction.repeatActionForever(
+        SKAction.sequence([
+          SKAction.runBlock(addMonster),SKAction.waitForDuration(1.0)
+        ])
+      ))
     }
+  
+  
+  func random()->CGFloat{
+    return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+  }
+  
+  func random_point(min:CGFloat, max:CGFloat) -> CGFloat{
+    return random()*(max-min)+min
+  }
+  
+  func addMonster(){
+  
+    //create sprite
+    let monster = SKSpriteNode(imageNamed: "monster")
     
+    
+    //determine where to spawn the monster along the Y axis
+    let actualY = random_point(monster.size.height/2, max: size.height - monster.size.height/2)
+  
+    //add the monster to the scene
+    monster.position = CGPoint(x:size.width + monster.size.width/2, y: actualY)
+    
+    addChild(monster)
+    
+    //determine speed of the monster
+    let actualDuration = random_point(CGFloat(5.0), max: CGFloat(5.0))
+    
+    //create the actions
+    let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y:actualY), duration: NSTimeInterval(actualDuration))
+    let actionMoveDone = SKAction.removeFromParent()
+    monster.runAction(SKAction.sequence([actionMove,actionMoveDone]))
+  }
+  
+  
+  /*
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
@@ -38,7 +81,7 @@ class GameScene: SKScene {
             self.addChild(sprite)
         }
     }
-   
+    */
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
